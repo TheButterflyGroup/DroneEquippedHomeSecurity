eec05c27d814689ac04ff184e3dd0997b51e3d35
Nestor Gomez Jimenez <gomez315@umn.edu>
2017-10-28 13:55:11 -0500

    info.html and info.controller complete

--- server/public/scripts/controllers/info.controller.js
+++ server/public/scripts/controllers/info.controller.js
@@ -1,25 +1,16 @@
 myApp.controller('InfoController', function (UserService) {
   console.log('InfoController created');
   var vm = this;
+  
   vm.userService = UserService;
 
-  vm.doorObject = { data: [] };
-  vm.motionObject = { data: [] };
-
-  vm.getDoorStatus = function () {
+  vm.sensorObject = { data: [] };
+  
+  vm.sensorStatus = function () {
     $http.get('/security/all').then(function (response) { 
-      vm.doorObject.data = response.data;
-      console.log('in getDoorStatus:', vm.doorObject.data);
+      vm.sensorObject.data = response.data;
+      console.log('in getDoorStatus:', vm.sensorObject.data);
     }); //end $http get
-  }//end getSights
-
-  vm.getMotionData = function () {
-    $http.get('/security/all').then(function (response) { 
-      vm.motionObject.data = response.data;
-      console.log('in getMotionData:', vm.motionObject.data);
-    }); //end $http get
-  }//end getSights
-
-
+  }//end sensorStatus
 
 });
