c54a9758b206c1d9d3b1a76944b3f74082592d78
ale-cia <ahodges230@gmail.com>
2017-10-28 13:12:39 -0500

    Created buttons and controller

---
+++ server/public/scripts/controllers/mock.controller.js
@@ -0,0 +1,14 @@
+myApp.controller('MockController', function(UserService) {
+    console.log('MockController created');
+    var vm = this;
+
+
+   vm.open = function(){
+
+   }
+
+
+
+
+  });
+  
---
+++ server/public/scripts/services/mock.service.js
@@ -0,0 +1,3 @@
+myApp.service('MockService', ['$http', function($http) {
+    console.log('Mock service loaded.');
+}]);
